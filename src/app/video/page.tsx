6b77e32672f7ff098d0ed4d552e8b4daf17d5c9c
Andres Castro <guevaracastroandres@gmail.com>
2024-02-03 00:47:18 -0700

    Lau portafolio first deploy

---
+++ src/app/video/page.tsx
@@ -0,0 +1,10 @@
+import Link from "../../../node_modules/next/link"
+
+export default function Video() {
+    return(
+        <>
+        <h1>This is a test</h1>
+        <Link href="www.google.com">Test</Link>
+        </>
+    )
+}
